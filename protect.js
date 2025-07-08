4afb32d98a609c6a73ed4d4427eceea56d03ce97
creative-Anshra <anshuqaimkhani@gmail.com>
2025-07-08 07:51:17 -0700

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,7 +1,7 @@
 function checkPassword() {
   const input = document.getElementById("password").value;
   if (input === "mylove26") {
-    window.location.href = "index.html"; // Redirect to your real birthday page
+    window.location.href = "main.html"; // Redirect to your real birthday page
   } else {
     alert("❌ Wrong password. Try again.");
   }
